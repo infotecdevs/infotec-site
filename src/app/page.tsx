8e7bade9229b750aec3ac3f0ac12746b30448957
fsRoger <fsroger@outlook.com>
2024-01-27 19:21:43 -0300

    Add skill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Hero from "@/components/Hero";
+import Skill from "@/components/Skill/index";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Skill />
+    </>
+
+  );
 }
