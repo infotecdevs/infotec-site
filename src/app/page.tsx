46e9be2e8be06445b060c7203d630ed02b57ca33
fsRoger <fsroger@outlook.com>
2024-01-27 19:42:45 -0300

    Add Experience Time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Skill from "@/components/Skill/index";
+import Experience from "@/components/Experience/index"
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Hero />
       <Skill />
+      <Experience />
     </div>
   );
 }
---
+++ src/components/Experience/index.jsx
@@ -0,0 +1,43 @@
+
+
+const Experience = () => {
+  return (
+    <div className="dark:bg-black text-white dark:text-white py-14">
+      <div className="container">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-3">
+          {/* first col */}
+          <div className="bg-dark/50 p-4 grid place-items-center">
+            <div className="text-center space-y-3">
+              <h1 className="text-5xl font-bold">12</h1>
+              <p className="text-xl font-semibold">Anos de Experiência</p>
+            </div>
+          </div>
+          {/* second col */}
+          <div className="grid grid-rows-2 gap-4">
+            <div className="text-center p-6 bg-dark">
+              <h1 className="text-3xl font-bold">60+</h1>
+              <p>Clientes Satisfeitos</p>
+            </div>
+            <div className="text-center p-6 bg-dark">
+              <h1 className="text-3xl font-bold">120+</h1>
+              <p>Projetos Completos</p>
+            </div>
+          </div>
+          {/* Third col */}
+          <div className="grid grid-rows-2 gap-4">
+            <div className="text-center p-6 bg-dark">
+              <h1 className="text-3xl font-bold">60+</h1>
+              <p>Clientes Satisfeitos</p>
+            </div>
+            <div className="text-center p-6 bg-dark">
+              <h1 className="text-3xl font-bold">120+</h1>
+              <p>Projetos Completos</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
